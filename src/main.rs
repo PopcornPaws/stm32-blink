2fd5358cf990aec93fbc51dd97cf831305dfe7fb
Mark Melczer <melczer7@gmail.com>
2020-02-12 23:21:17 +0100

    Slowly getting to know what means what.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+extern crate cortex_m_rt;
+extern crate panic_halt;
+extern crate stm32f3;
+
+use cortex_m_rt::entry;
+use stm32f3::stm32f303;
+
+// Use main as the entry point for this application
+#[entry]
+fn main() -> ! {
+    // Get handles to the hardware
+    let peripherals = stm32f303::Peripherals::take().unwrap();
+    let gpioa = &peripherals.GPIOA;
+    let gpioe = &peripherals.GPIOE;
+    let rcc = &peripherals.RCC;
+
+    // Enable GPIO clock for IO ports A and E
+    rcc.abp2enr.modify(|_, w| {
+        w.iopaen().enabled();
+        w.iopeen().enabled();
+    });
+    
 }
