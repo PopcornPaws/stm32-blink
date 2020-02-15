2c82e254d8ad3b9a5ce0b242f3a04ea9ca70f9a4
Mark Melczer <melczer7@gmail.com>
2020-02-15 14:55:24 +0100

    Nice notes.

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,11 @@ use stm32f3::stm32f303;
 fn main() -> ! {
     // Get handles to the hardware
     let peripherals = stm32f303::Peripherals::take().unwrap();
-    let gpioa = &peripherals.GPIOA;
-    let gpioe = &peripherals.GPIOE;
+    let gpioa = &peripherals.GPIOA; // Push button is accessed via A0
+    let gpioe = &peripherals.GPIOE; // Led is accessed via E15
     let rcc = &peripherals.RCC;
 
-    // Enable GPIO clock for IO ports A and E
+    // Enable GPIO clock for IO ports A and E on ABP2 Clock
     rcc.abp2enr.modify(|_, w| {
         w.iopaen().enabled();
         w.iopeen().enabled();
